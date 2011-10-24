85cbdd5940521571d5cdfaa97d85fc8b8ab5c957
Matt Hill <matt.o.hill@gmail.com>
2011-10-23 22:23:49 -0700

    user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111023035954) do
+ActiveRecord::Schema.define(:version => 20111024032322) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
